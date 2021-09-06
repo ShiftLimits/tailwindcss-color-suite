080252a0a46c503fc3eb1426f389df497797c59e
Jeff Schofield <jeff@jeffschofield.com>
2021-09-06 12:48:07 -0400

    feat(editor): Add z-index and transition-duration utils

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 const colors = require('tailwindcss/colors')
+function csZ(index) {
+  return 99999 + index
+}
 
 module.exports = {
   purge: [
@@ -14,7 +17,20 @@ module.exports = {
         gray: colors.gray
       },
       fill: (theme) => theme('colors'),
-      stroke: (theme) => theme('colors')
+      stroke: (theme) => theme('colors'),
+      transitionDuration: {
+          '25': '25ms', // might as well be instant
+          '35': '35ms', // barely perceptible
+          '50': '50ms',
+          '2000': '2000ms',
+          '3000': '3000ms'
+      },
+      zIndex: {
+        'under-panel': csZ(40),
+        'panel': csZ(50),
+        'over-panel': csZ(60),
+        'modal': csZ(100)
+      }
     },
   },
   variants: {
