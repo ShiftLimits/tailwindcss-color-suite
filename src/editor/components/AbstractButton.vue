a8097646ab5377d34ece6274f7b83f77b811b891
Jeff Schofield <jeff@jeffschofield.com>
2021-09-08 10:55:19 -0400

    feat(editor): Add abstract button base

---
+++ src/editor/components/AbstractButton.vue
@@ -0,0 +1,20 @@
+<template>
+	<component :is="to ? 'router-link' : 'button'" v-bind="$attrs" :to="to" v-slot="slot" :custom="!!to && !$attrs.class">
+		<slot
+			:isActive="slot ? slot.isActive : null"
+			:href="slot ? slot.href : null"
+			:navigate="slot ? slot.navigate : () => {}"
+		/>
+	</component>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	props: ['to'],
+	setup() {
+		return {}
+	}
+})
+</script>
