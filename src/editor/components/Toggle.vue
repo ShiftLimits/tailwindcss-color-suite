f1a14bc18188eec3d9212a42091b8b8bc92cbd77
Jeff Schofield <jeff@jeffschofield.com>
2021-09-09 16:59:45 -0400

    refactor(editor): Improved syncing logic to prevent double updates

--- src/editor/components/Toggle.vue
+++ src/editor/components/Toggle.vue
@@ -7,24 +7,30 @@
 		<div class="__cs-absolute __cs-inset-0 __cs-transition-transform __cs-duration-100 __cs-ease-in-out __cs-transform" :class="{ '__cs-translate-x-full': value }">
 			<div class="__cs-w-4 __cs-h-4 __cs-bg-white __cs-rounded-full __cs-transition-transform __cs-duration-100 __cs-ease-in-out __cs-transform" :class="{ '__cs--translate-x-full': value }"></div>
 		</div>
-		<input class="__cs-opacity-0 __cs-absolute __cs-inset-0 __cs-cursor-pointer __cs-outline-none" type="checkbox" v-bind="$attrs" v-model="value" />
+		<input class="__cs-opacity-0 __cs-absolute __cs-inset-0 __cs-cursor-pointer __cs-outline-none" type="checkbox" v-model="value" />
 	</div>
 </template>
 
 <script lang="ts">
-	import { computed, defineComponent } from 'vue'
+	import { computed, defineComponent, nextTick, ref, watch } from 'vue'
 
 	export default defineComponent({
 		props: ['modelValue'],
 		setup (props, { emit }) {
-			const value = computed<string>({
-				get() {
-					return props.modelValue
-				},
-				set(value) {
-					emit('update:modelValue', value)
-				}
-			})
+			const value = ref<string>('')
+
+			const flush = 'sync'
+			let syncing = false
+			watch(() => props.modelValue, (new_value) => {
+				syncing = true // Mark as syncing so when we set the internal value it doesn't also emit again
+				value.value = new_value
+				nextTick(() => syncing = false) // Mark as not syncing on the next tick; since flush is 'sync' both watches will be processed at once; after that we need not be marked as syncing
+			}, { flush, immediate: true })
+			watch(value, (new_value) => {
+				if (syncing) syncing = false
+				else emit('update:modelValue', new_value)
+			}, { flush, immediate: false })
+
 			return {
 				value
 			}
