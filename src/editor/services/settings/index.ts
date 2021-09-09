94e85165a6515570fa2936598baf7d0af126625b
Jeff Schofield <jeff@jeffschofield.com>
2021-09-09 19:34:28 -0400

    feat(editor): Update settings service to use store

--- src/editor/services/settings/index.ts
+++ src/editor/services/settings/index.ts
@@ -1,6 +1,9 @@
-import { inject, reactive } from 'vue'
+import { inject, reactive, computed, toRaw, triggerRef, toRef } from 'vue'
 import { SettingsService } from './types'
 import { ColorSuiteSettings } from '../../../types'
+import { Store } from 'vuex'
+import { settings_store } from './store'
+import { COLOR_SUITE_ID, DEFAULT_COLOR_CONFIG } from '../../../constants';
 
 const settings_service_key = Symbol('__SETTINGS_SERVICE__')
 
@@ -10,19 +13,31 @@ export function useSettingsService() {
 	return settings_service
 }
 
-export function createSettingsService() {
-	const settings = reactive<ColorSuiteSettings>({
-		open_button: true,
-		float_panel: false,
-		include_transparent: true,
-		include_current: true
+export function createSettingsService(store:Store<any>) {
+	const settings = new Proxy<ColorSuiteSettings>({} as ColorSuiteSettings, {
+		get(target, prop) {
+			return store.state.settings[prop]
+		},
+		set(target, prop, value) {
+			store.dispatch('settings/update', { [prop]: value })
+			return true
+		}
 	})
+
+	if (import.meta.hot) {
+		import.meta.hot.on(`${ COLOR_SUITE_ID }:config-updated` as any, (config:any) => {
+			Object.assign(settings, config.settings)
+		})
+	}
 
 	const settings_service:SettingsService = {
 		settings,
 		install(app) {
+			store.registerModule('settings', settings_store)
 			app.provide(settings_service_key, settings_service)
 		}
 	}
 	return settings_service
 }
+
+export * from './forms'
