c535ebc330cc438d04d273dd3d67d6b95ca4bf8e
Jeff Schofield <jeff@jeffschofield.com>
2021-09-13 03:21:53 -0400

    refactor: Make explicit types and ifaces for color suite colors

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,26 @@
+import { Point } from './editor/lib/point'
+import { ColorHSVA } from './editor/lib/color'
+
+
+export type CSColorSolid = ColorHSVA // Store Solid as HSVA representation
+
 export interface CSColorScale {
+	use_hue_curve:boolean
+	hue_curve:CSComponentCurve
+	saturation_curve:CSComponentCurve
+	value_curve:CSComponentCurve
 
 	start:number
 	end:number
 	steps:number
 }
 
-export interface CSColorAlias {
-	value:string
+export type CSColorAlias = string // `colors.example.500`
+export interface CSColorAliasResolutionError {
+	message?:string
 }
 
-export type CSColor = CSColorAlias|CSColorScale|string
+export type CSColor = CSColorAlias|CSColorScale|CSColorSolid
 
 export interface ColorSuiteSettings {
 	open_button:boolean
@@ -18,7 +29,7 @@ export interface ColorSuiteSettings {
 	include_current:boolean
 }
 
-export type ColorSuiteColors = CSColor[]
+export type ColorSuiteColors = {[key:string]:CSColor}
 
 export interface ColorSuiteConfig {
 	colors: ColorSuiteColors,
