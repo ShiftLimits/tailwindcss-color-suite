24f3a6b98d76fe4fdf95479a121bc7e186bcc63e
Jeff Schofield <jeff@jeffschofield.com>
2021-09-16 02:29:22 -0400

    feat: Add `colorScaleTokens` function to color scale lib for getting only the tokens of a scale

--- src/editor/lib/color-scale/utils.ts
+++ src/editor/lib/color-scale/utils.ts
@@ -2,6 +2,18 @@ import { CSColorScale, CSColor } from '../../../types';
 import { ColorHSV, ColorHSVA, ColorHEX, ColorRGBA, hsvaToHex, hsvaToRGBA } from '../color'
 import { sampleComponentCurve } from '../component-curve'
 
+export function colorScaleTokens(color_scale:CSColorScale) {
+	let tokens:string[] = []
+
+	for (let i = 0; i < color_scale.steps; i++) {
+		let step = i / (color_scale.steps-1)
+		let interval = Math.round(step * color_scale.start + (1-step) * color_scale.end)
+
+		tokens.push(interval.toString())
+	}
+	return tokens
+}
+
 export function samplePanelColorScale(color_scale:CSColorScale, y:number):ColorHSV {
 	let hue = sampleComponentCurve(color_scale.hue_curve , y, 360)
 	let saturation = sampleComponentCurve(color_scale.saturation_curve, y)
