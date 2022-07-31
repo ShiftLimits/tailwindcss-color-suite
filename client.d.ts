c9f8a32ad70c9b8dbd9ed4c4dd97219bbce026d2
Jeff Schofield <jeff@jeffschofield.com>
2022-07-31 14:23:56 -0400

    feat: Add declarations for virtual config imports

--- client.d.ts
+++ client.d.ts
@@ -1,4 +1,14 @@
-﻿declare module 'virtual:color-suite-colors' {
+﻿declare module 'virtual:color-suite/colors' {
   const colors: {[key: string]: string | {[key: string]: string }}
   export default colors
+}
+
+declare module 'virtual:color-suite/config/colors' {
+  const colors: {[key: string]: string | {[key: string]: string }}
+  export default colors
+}
+
+declare module 'virtual:color-suite/config/settings' {
+  const settings: {[key: string]: any}
+  export default settings
 }
